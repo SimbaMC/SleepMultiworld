32eb35da79f846e30a154e8e8580b0d5aaf40dbe
Simba <simba98@live.com>
2023-02-04 12:46:21 +0800

    Mark as server-side only

--- src/main/java/simba/sleepmultiworld/sleepmultiworld.java
+++ src/main/java/simba/sleepmultiworld/sleepmultiworld.java
@@ -1,6 +1,9 @@
 package simba.sleepmultiworld;
 
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.NetworkConstants;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("sleepmultiworld")
@@ -8,6 +11,8 @@ public class sleepmultiworld
 {
     public sleepmultiworld()
     {
+        //Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 
 }
