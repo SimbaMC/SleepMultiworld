02899952c4c704bcc47b42d5a3b7d8650f2cb81d
Simba <simba98@live.com>
2023-02-16 13:30:52 +0800

    Init mixin

---
+++ src/main/java/simba/sleepmultiworld/mixins/wayStoneXp_mixin.java
@@ -0,0 +1,8 @@
+package simba.sleepmultiworld.mixins;
+
+import net.blay09.mods.waystones.core.PlayerWaystoneManager;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(PlayerWaystoneManager.class)
+public class wayStoneXp_mixin {
+}
