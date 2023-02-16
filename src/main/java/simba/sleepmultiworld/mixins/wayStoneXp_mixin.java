d4606a221c2ee36b8e6b95d7ad3ee79a012eeb8f
Simba <simba98@live.com>
2023-02-16 22:41:33 +0800

    Test Mixin modify

--- src/main/java/simba/sleepmultiworld/mixins/wayStoneXp_mixin.java
+++ src/main/java/simba/sleepmultiworld/mixins/wayStoneXp_mixin.java
@@ -2,7 +2,36 @@ package simba.sleepmultiworld.mixins;
 
 import net.blay09.mods.waystones.core.PlayerWaystoneManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Slice;
 
 @Mixin(PlayerWaystoneManager.class)
 public class wayStoneXp_mixin {
+
+    @ModifyVariable(
+            method = "getExperienceLevelCost(Lnet/minecraft/world/entity/Entity;Lnet/blay09/mods/waystones/api/IWaystone;Lnet/blay09/mods/waystones/core/WarpMode;Lnet/blay09/mods/waystones/core/WaystoneTeleportContext;)I",
+            at = @At(
+                    value = "INVOKE_ASSIGN",
+                    target = "Lnet/minecraft/util/Mth;clamp(DDD)D"
+            ),
+            require = 1,
+            allow = 1,
+            name = "dimensionalWarpXpCost",
+            slice = @Slice(
+                    from = @At(
+                            value = "INVOKE",
+                            target = "Lnet/blay09/mods/waystones/config/WaystonesConfigData;dimensionalWarpXpCost()I"
+                    ),
+                    to = @At(
+                            value = "INVOKE",
+                            target = "Lnet/blay09/mods/waystones/config/WaystonesConfigData;blocksPerXPLevel()I",
+                            ordinal = 0
+                    )
+            )
+    )
+    static int coordinate_scale_addon_mixin(int dimensionalWarpXpCost){
+        return 0;
+    }
+
 }
